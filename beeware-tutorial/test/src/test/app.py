107c717b01580a8720cb31851260d7045dfaf81b
Juphet Vitualla <jvitualla@dnamicro.com>
2021-01-27 09:02:58 +0800

    adde event

--- beeware-tutorial/test/src/test/app.py
+++ beeware-tutorial/test/src/test/app.py
@@ -4,54 +4,63 @@ this is test
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-import httpx
+
 
 class test(toga.App):
 
     def startup(self):
-        """
-        Construct and show the Toga application.
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
+
+        
+
+        box1 = toga.Box()
+        box2 = toga.Box()
+        box3 = toga.Box()
+        box4 = toga.Box()
+        box5 = toga.Box()
+        box6 = toga.Box()
+
         main_box = toga.Box()
 
-        location_label = toga.Label("Location", style=Pack(padding=(0, 5)))
-        self.location_input = toga.TextInput(style=Pack(flex=1))
+        Label1 = toga.Label("Unit 1")
+        self.input_text = toga.TextInput()
+        Label2 = toga.Label("Unit 2")
+        self.input_text2 = toga.TextInput()
+        self.input_text.style.width = 400
+        self.input_text.style.padding_top = 10
+        self.input_text2.style.width = 400
+        self.input_text2.style.padding_top = 10
 
-        location_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        location_box.add(location_label)
-        location_box.add(self.location_input)
+        Label3 = toga.Label("Result: ")
+        box6.add(Label3)
 
-        weather_box_label = toga.Label("Weather Results", style=Pack(padding=(0, 5)))
-        self.weather_box_input = toga.TextInput(readonly=True, style=Pack(flex=1))
-        weather_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        weather_box.add(weather_box_label)
-        weather_box.add(self.weather_box_input)
+        def button_handler(widget):
+            Label3.text = "Result: {}!".format(input_text.value)
 
-        button = toga.Button("Fetch weather", on_press=self.weather, style=Pack(padding=5))
+        button1 = toga.Button("Convert Unit", on_press=button_handler)
+        button1.style.padding_top = 10
 
-        main_box.add(location_box)
-        main_box.add(button)
+        box1.add(Label1)
+        box2.add(self.input_text)
+        box3.add(Label2)
+        box4.add(self.input_text2)
+        box5.add(button1)
 
+        
 
-        self.main_window = toga.MainWindow(title="this is sample")
+        main_box.add(box1)
+        main_box.add(box2)
+        main_box.add(box3)
+        main_box.add(box4)
+        main_box.add(box5)
+
+        main_box.style.update(direction=COLUMN)
+
+        
+
+        self.main_window = toga.MainWindow(title="Unit Converter")
         self.main_window.content = main_box
         self.main_window.show()
-
-    def weather(self, widget):
-        params = dict(access_key=TOKEN, query=self.location_input.value)
-        resp = httpx.get(BASE_URL, params=params).json()
-        try:
-            self.weather_box_input.value = f'The weather report for  {resp["request"]["query"]} \
-            {resp["current"]["temperature"]}  \
-            {resp["current"]["weather_descriptions"][0]} \
-            {resp["current"]["feelslike"]}'
-        except ValueError:
-            self.weather_box_input.value = "Location unknown!"
-
 
 def main():
     return test()
